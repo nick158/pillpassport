898bb61e03e31e8605918173aeba959112610764
Andrew12Liu <andrew121999@hotmail.com>
2018-09-15 11:51:28 -0400

    scraped old backend for new backend

---
+++ backend/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+router.post('/', function(req, res, next) {
+  writeUserData
+});
+
+
+
+
+module.exports = router;
