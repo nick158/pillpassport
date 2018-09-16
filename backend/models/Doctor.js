e7b4918a10e34c147a7ef9d7d3390c44b0272e9a
Unknown <andrew121999@hotmail.com>
2018-09-15 22:48:26 -0400

    add doctor

---
+++ backend/models/Doctor.js
@@ -0,0 +1,12 @@
+const Model = require('./Model');
+/** Patient Model
+presc: [{}]
+name: string
+Doctors: [id]
+*/
+class Doctor extends Model {
+  constructor(data){
+    data = data || {};
+
+  }
+}
